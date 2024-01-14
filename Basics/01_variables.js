886bf48defbc39cb63798768f244015f616186e8
KanoujiaShreya <67023012+KanoujiaShreya@users.noreply.github.com>
2024-01-14 15:47:45 +0000

    datatypes

---
+++ Basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 213;
+let accountEmail = "shreya@google.com";
+var accountPassword = 654321;
+accountCity = "Kanpur"
+let State;
+//accountId = 123;
+
+//assignment to constant variable is not allowed.
+
+accountEmail = "sk@google.com"
+accountPassword = 345;
+accountCity = "Jaipur"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, State])
+
+/*
+
+let and var is same and used for declaring variable.
+
+But prefer not to use var because of issue in block scope and functional scope.
+
+*/
---
+++ Basics/02_datatypes.js
@@ -0,0 +1,19 @@
+//(tc39 , mdn) - documentations
+let name = "shreya"
+let age = 22
+let isLoggedIn = true
+let city
+let state = null
+
+/*
+string = ""
+number = 2 to power 53
+boolean = true/false
+null = standalone value
+undefined
+*/
+
+console.log(typeof null)
+console.log(typeof undefined)
+
+
